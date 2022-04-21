0a839cd62fd6c991af4dd94e815193d61d984ce4
Jason <timeli@naver.com>
2022-04-21 22:10:44 +0900

    add quotes and background

--- clone_coding/js/clock.js
+++ clone_coding/js/clock.js
@@ -2,9 +2,14 @@ const clock = document.querySelector('h2#clock');
 
 function getClock() {
   const date = new Date();
-  let hours = String(date.getHours()).padStart(2, '0');
-  let minutes = String(date.getMinutes()).padStart(2, '0');
-  let seconds = String(date.getSeconds()).padStart(2, '0');
+  //let hours = String(date.getHours()).padStart(2, '0');
+  let hours = `${
+    date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+  } `;
+  let minutes = String(date.getMinutes()).padStart(2, '0'); // 이렇게 해도 됨
+  let seconds = `${
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+  } `;
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
